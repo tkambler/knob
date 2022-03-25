7f67265599f3fea2f45377601932eb34358e7b79
Tim Ambler <tkambler@gmail.com>
2022-03-25 08:42:43 -0500

    install script

--- install.sh
+++ install.sh
@@ -22,6 +22,51 @@ has_curl() {
     fi
 }
 
+is_x64arch() {
+    local HARDWARE_NAME
+    HARDWARE_NAME=$(uname -m)
+    if [[ $HARDWARE_NAME == *"x86_64"* ]]; then
+        echo "1"
+    else
+        echo "0"
+    fi
+}
+
+get_platform() {
+    local unameOut
+    unameOut="$(uname -s)"
+    case "${unameOut}" in
+        Linux*)     machine=Linux;;
+        Darwin*)    machine=Mac;;
+        *)          machine="UNKNOWN:${unameOut}"
+    esac
+    echo ${machine}
+}
+
+get_bin() {
+    local PLATFORM
+    local IS_X64
+    PLATFORM=$(get_platform)
+    IS_X64=$(is_x64arch)
+    if [ $PLATFORM == "Mac" ]
+    then
+        if [ $IS_X64 == "1" ]; then
+            echo "knob-macos-x64"
+        else
+            echo "The install script only supports x64 architecture at this time. A PR that fixes this would be welcome."
+            knob_reset
+            exit 1
+        fi
+    elif [ $PLATFORM == "Linux" ]
+    then
+        echo "knob-linux-x64"
+    else
+        echo "This script does not support platform: $PLATFORM"
+        knob_reset
+        exit 1
+    fi
+}
+
 is_root() {
     if [[ "$EUID" = 0 ]]; then
         echo 1
@@ -31,12 +76,16 @@ is_root() {
 }
 
 dl_wget() {
-    wget --quiet --show-progress https://github.com/tkambler/knob/releases/download/$KNOB_VERSION/knob-macos-x64 -O /tmp/knob
+    local KNOB_BIN
+    KNOB_BIN=$(get_bin)
+    wget --quiet --show-progress https://github.com/tkambler/knob/releases/download/$KNOB_VERSION/${KNOB_BIN} -O /tmp/knob
     chmod +x /tmp/knob
 }
 
 dl_curl() {
-    curl --silent --progress-bar -o /tmp/knob https://github.com/tkambler/knob/releases/download/$KNOB_VERSION/knob-macos-x64
+    local KNOB_BIN
+    KNOB_BIN=$(get_bin)
+    curl --silent --progress-bar -o /tmp/knob https://github.com/tkambler/knob/releases/download/$KNOB_VERSION/${KNOB_BIN}
 }
 
 dl_knob() {
@@ -48,6 +97,7 @@ dl_knob() {
         dl_curl
     else
         echo "Error: Unable to find wget or curl executable"
+        knob_reset
         exit 1
     fi
 }
@@ -81,7 +131,7 @@ knob_do_install() {
 }
 
 knob_reset() {
-    unset -f has_wget has_curl is_root dl_wget dl_curl dl_knob set_perms move_bin on_success knob_do_install knob_reset
+    unset -f has_wget has_curl is_root dl_wget dl_curl dl_knob set_perms move_bin on_success knob_do_install get_platform get_bin is_x64arch knob_reset
     unset -v KNOB_VERSION
 }
 
